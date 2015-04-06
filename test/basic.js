e80cfefccfbccd6db9ce51750ba8972111682c88
Rebecca Turner <me@re-becca.org>
2015-04-06 17:34:24 -0400

    Change formatting to standard

--- test/basic.js
+++ test/basic.js
@@ -1,15 +1,15 @@
-"use strict"
-var HostedGit = require("../index")
-var test = require("tap").test
+'use strict'
+var HostedGit = require('../index')
+var test = require('tap').test
 
-test("basic", function (t) {
-   t.is(HostedGit.fromUrl("https://google.com"), undefined, "null on failure")
-   t.is(HostedGit.fromUrl("https://github.com/abc/def").getDefaultType(), "https", "match https urls")
-   t.is(HostedGit.fromUrl("ssh://git@github.com/abc/def").getDefaultType(), "sshurl", "match ssh urls")
-   t.is(HostedGit.fromUrl("git+ssh://git@github.com/abc/def").getDefaultType(), "sshurl", "match git+ssh urls")
-   t.is(HostedGit.fromUrl("git+https://github.com/abc/def").getDefaultType(), "https", "match git+https urls")
-   t.is(HostedGit.fromUrl("git@github.com:abc/def").getDefaultType(), "sshurl", "match ssh connect strings")
-   t.is(HostedGit.fromUrl("git://github.com/abc/def").getDefaultType(), "git", "match git urls")
-   t.is(HostedGit.fromUrl("github:abc/def").getDefaultType(), "shortcut", "match shortcuts")
-   t.end()
+test('basic', function (t) {
+  t.is(HostedGit.fromUrl('https://google.com'), undefined, 'null on failure')
+  t.is(HostedGit.fromUrl('https://github.com/abc/def').getDefaultType(), 'https', 'match https urls')
+  t.is(HostedGit.fromUrl('ssh://git@github.com/abc/def').getDefaultType(), 'sshurl', 'match ssh urls')
+  t.is(HostedGit.fromUrl('git+ssh://git@github.com/abc/def').getDefaultType(), 'sshurl', 'match git+ssh urls')
+  t.is(HostedGit.fromUrl('git+https://github.com/abc/def').getDefaultType(), 'https', 'match git+https urls')
+  t.is(HostedGit.fromUrl('git@github.com:abc/def').getDefaultType(), 'sshurl', 'match ssh connect strings')
+  t.is(HostedGit.fromUrl('git://github.com/abc/def').getDefaultType(), 'git', 'match git urls')
+  t.is(HostedGit.fromUrl('github:abc/def').getDefaultType(), 'shortcut', 'match shortcuts')
+  t.end()
 })
