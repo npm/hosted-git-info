fd4e008cb1ec738043fe85bf94c30cd4a5a712fc
Rebecca Turner <me@re-becca.org>
2015-04-09 13:46:18 -0400

    Separate into multiple files
    To split the data, class and factory

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 'use strict'
 var url = require('url')
-
-var GitHost = exports = module.exports = function (type, user, auth, project, comittish, defaultType) {
-  var gitHostInfo = this
-  gitHostInfo.type = type
-  Object.keys(gitHosts[type]).forEach(function (key) {
-    gitHostInfo[key] = gitHosts[type][key]
-  })
-  gitHostInfo.user = user
-  gitHostInfo.auth = auth
-  gitHostInfo.project = project
-  gitHostInfo.comittish = comittish
-  gitHostInfo.default = defaultType
-}
-GitHost.prototype = {}
+var gitHosts = require('./git-host-info.js')
+var GitHost = module.exports = require('./git-host.js')
 
 var protocolMap = {
   'git+ssh': 'sshurl',
@@ -33,7 +21,7 @@ var authProtocols = {
   'http:': true
 }
 
-exports.fromUrl = function (giturl) {
+module.exports.fromUrl = function (giturl) {
   if (giturl == null || giturl === '') return
   var parsed = parseGitUrl(maybeGitHubShorthand(giturl) ? 'github:' + giturl : giturl)
   var matches = Object.keys(gitHosts).map(function (gitHostName) {
@@ -49,7 +37,7 @@ exports.fromUrl = function (giturl) {
     }
     if (parsed.host !== gitHostInfo.domain) return
     if (!gitHostInfo.protocols_re.test(parsed.protocol)) return
-    var pathmatch = gitHostInfo.pathmatch || gitHostDefaults.pathmatch
+    var pathmatch = gitHostInfo.pathmatch
     var matched = parsed.path.match(pathmatch)
     if (!matched) return
     return new GitHost(
@@ -76,7 +64,7 @@ function maybeGitHubShorthand (arg) {
   return /^[^:@%/\s.-][^:@%/\s]*[/][^:@\s/%]+(?:#.*)?$/.test(arg)
 }
 
-var parseGitUrl = function (giturl) {
+function parseGitUrl (giturl) {
   if (typeof giturl !== 'string') giturl = '' + giturl
   var matched = giturl.match(/^([^@]+)@([^:]+):[/]?((?:[^/]+[/])?[^/]+?)(?:[.]git)?(#.*)?$/)
   if (!matched) return url.parse(giturl)
@@ -94,146 +82,4 @@ var parseGitUrl = function (giturl) {
     path: '/' + matched[3],
     href: 'git+ssh://' + matched[1] + '@' + matched[2] + '/' + matched[3] + (matched[4] || '')
   }
-}
-
-var gitHostDefaults = {
-  'sshtemplate': 'git@{domain}:{user}/{project}.git{#comittish}',
-  'sshurltemplate': 'git+ssh://git@{domain}/{user}/{project}.git{#comittish}',
-  'browsetemplate': 'https://{domain}/{user}/{project}{/tree/comittish}',
-  'docstemplate': 'https://{domain}/{user}/{project}{/tree/comittish}#readme',
-  'httpstemplate': 'git+https://{auth@}{domain}/{user}/{project}.git{#comittish}',
-  'filetemplate': 'https://{domain}/{user}/{project}/raw/{comittish}/{path}',
-  'shortcuttemplate': '{type}:{user}/{project}{#comittish}',
-  'pathtemplate': '{user}/{project}{#comittish}',
-  'pathmatch': /^[/]([^/]+)[/]([^/]+?)(?:[.]git)?$/
-}
-var gitHosts = {
-  github: {
-    // First two are insecure and generally shouldn't be used any more, but
-    // they are still supported.
-    'protocols': [ 'git', 'http', 'git+ssh', 'git+https', 'ssh', 'https' ],
-    'domain': 'github.com',
-    'treepath': 'tree',
-    'filetemplate': 'https://{auth@}raw.githubusercontent.com/{user}/{project}/{comittish}/{path}',
-    'bugstemplate': 'https://{domain}/{user}/{project}/issues',
-    'gittemplate': 'git://{auth@}{domain}/{user}/{project}.git{#comittish}'
-  },
-  bitbucket: {
-    'protocols': [ 'git+ssh', 'git+https', 'ssh', 'https' ],
-    'domain': 'bitbucket.org',
-    'treepath': 'src'
-  },
-  gitlab: {
-    'protocols': [ 'git+ssh', 'git+https', 'ssh', 'https' ],
-    'domain': 'gitlab.com',
-    'treepath': 'tree',
-    'docstemplate': 'https://{domain}/{user}/{project}{/tree/comittish}#README',
-    'bugstemplate': 'https://{domain}/{user}/{project}/issues'
-  },
-  gist: {
-    'protocols': [ 'git', 'git+ssh', 'git+https', 'ssh', 'https' ],
-    'domain': 'gist.github.com',
-    'pathmatch': /^[/](?:([^/]+)[/])?([a-z0-9]+)(?:[.]git)?$/,
-    'filetemplate': 'https://gist.githubusercontent.com/{user}/{project}/raw{/comittish}/{path}',
-    'bugstemplate': 'https://{domain}/{project}',
-    'gittemplate': 'git://{domain}/{project}.git{#comittish}',
-    'sshtemplate': 'git@{domain}:/{project}.git{#comittish}',
-    'sshurltemplate': 'git+ssh://git@{domain}/{project}.git{#comittish}',
-    'browsetemplate': 'https://{domain}/{project}{/comittish}',
-    'docstemplate': 'https://{domain}/{project}{/comittish}',
-    'httpstemplate': 'git+https://{domain}/{project}.git{#comittish}',
-    'shortcuttemplate': '{type}:{project}{#comittish}',
-    'pathtemplate': '{project}{#comittish}'
-  }
-}
-
-Object.keys(gitHosts).forEach(function (host) {
-  gitHosts[host].protocols_re = RegExp('^(' +
-    gitHosts[host].protocols.map(function (protocol) {
-      return protocol.replace(/([\\+*{}()\[\]$^|])/g, '\\$1')
-    }).join('|') + '):$')
-})
-
-GitHost.prototype.hash = function () {
-  return this.comittish ? '#' + this.comittish : ''
-}
-
-GitHost.prototype._fill = function (template, vars) {
-  if (!template) return
-  if (!vars) vars = {}
-  var self = this
-  Object.keys(this).forEach(function (key) { if (self[key] != null && vars[key] == null) vars[key] = self[key] })
-  var rawAuth = vars.auth
-  var rawComittish = vars.comittish
-  Object.keys(vars).forEach(function (key) { (key[0] !== '#') && (vars[key] = encodeURIComponent(vars[key])) })
-  vars['auth@'] = rawAuth ? rawAuth + '@' : ''
-  vars['#comittish'] = rawComittish ? '#' + rawComittish : ''
-  vars['/tree/comittish'] = vars.comittish ? '/' + vars.treepath + '/' + vars.comittish : ''
-  vars['/comittish'] = vars.comittish ? '/' + vars.comittish : ''
-  vars.comittish = vars.comittish || 'master'
-  var res = template
-  Object.keys(vars).forEach(function (key) {
-    res = res.replace(new RegExp('[{]' + key + '[}]', 'g'), vars[key])
-  })
-  return res
-}
-
-GitHost.prototype.ssh = function () {
-  var sshtemplate = this.sshtemplate || gitHostDefaults.sshtemplate
-  return this._fill(sshtemplate)
-}
-
-GitHost.prototype.sshurl = function () {
-  var sshurltemplate = this.sshurltemplate || gitHostDefaults.sshurltemplate
-  return this._fill(sshurltemplate)
-}
-
-GitHost.prototype.browse = function () {
-  var browsetemplate = this.browsetemplate || gitHostDefaults.browsetemplate
-  return this._fill(browsetemplate)
-}
-
-GitHost.prototype.docs = function () {
-  var docstemplate = this.docstemplate || gitHostDefaults.docstemplate
-  return this._fill(docstemplate)
-}
-
-GitHost.prototype.bugs = function () {
-  var bugstemplate = this.bugstemplate || gitHostDefaults.bugstemplate
-  return this._fill(bugstemplate)
-}
-
-GitHost.prototype.https = function () {
-  var httpstemplate = this.httpstemplate || gitHostDefaults.httpstemplate
-  return this._fill(httpstemplate)
-}
-
-GitHost.prototype.git = function () {
-  var gittemplate = this.gittemplate || gitHostDefaults.gittemplate
-  return this._fill(gittemplate)
-}
-
-GitHost.prototype.shortcut = function () {
-  var shortcuttemplate = this.shortcuttemplate || gitHostDefaults.shortcuttemplate
-  return this._fill(shortcuttemplate)
-}
-
-GitHost.prototype.path = function () {
-  var pathtemplate = this.pathtemplate || gitHostDefaults.pathtemplate
-  return this._fill(pathtemplate)
-}
-
-GitHost.prototype.file = function (P) {
-  var filetemplate = this.filetemplate || gitHostDefaults.filetemplate
-  return this._fill(filetemplate, {
-    path: P.replace(/^[/]+/g, '')
-  })
-}
-
-GitHost.prototype.getDefaultRepresentation = function () {
-  return this.default
-}
-
-GitHost.prototype.toString = function () {
-  return (this[this.default] || this.sshurl).call(this)
 }
