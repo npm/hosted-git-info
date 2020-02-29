0aaac77818415380550d489fe815e1d282014931
Darcy Clarke <darcy@darcyclarke.me>
2020-02-29 16:45:32 -0500

    fix: #61 & #65 addressing issues w/ url.URL implmentation which regressed node 6 support

--- test/auth.js
+++ test/auth.js
@@ -2,23 +2,25 @@ var HostedGitInfo = require('../')
 
 var tap = require('tap')
 var url = require('url')
-
-// Auth credentials with special characters (colon and/or at-sign) should remain correctly escaped
-var parsedInfo = HostedGitInfo.fromUrl('https://user%3An%40me:p%40ss%3Aword@github.com/npm/hosted-git-info.git')
-tap.equal(parsedInfo.auth, 'user%3An%40me:p%40ss%3Aword')
+var parsedInfo
 
 // Node.js' built-in `url` module should be able to parse the resulting url
-var parsedUrl = new url.URL(parsedInfo.toString())
-tap.equal(parsedUrl.username, 'user%3An%40me')
-tap.equal(parsedUrl.password, 'p%40ss%3Aword')
-tap.equal(parsedUrl.hostname, 'github.com')
-
-// For full backwards-compatibility; support auth where only username or only password is provided
-tap.equal(HostedGitInfo.fromUrl('https://user%3An%40me@github.com/npm/hosted-git-info.git').auth, 'user%3An%40me')
-tap.equal(HostedGitInfo.fromUrl('https://:p%40ss%3Aword@github.com/npm/hosted-git-info.git').auth, ':p%40ss%3Aword')
-
 // don't try to url.URL parse it if url.URL is not available
 // ie, node <6.13.  This is broken, but at least it doesn't throw.
-url.URL = null
-var parsedInfoNoURL = HostedGitInfo.fromUrl('https://user%3An%40me:p%40ss%3Aword@github.com/npm/xyz.git')
-tap.equal(parsedInfoNoURL.auth, 'user:n@me:p@ss:word')
+if (typeof url.URL === 'function') {
+  // Auth credentials with special characters (colon and/or at-sign) should remain correctly escaped
+  parsedInfo = HostedGitInfo.fromUrl('https://user%3An%40me:p%40ss%3Aword@github.com/npm/hosted-git-info.git')
+  tap.equal(parsedInfo.auth, 'user%3An%40me:p%40ss%3Aword')
+
+  var parsedUrl = new url.URL(parsedInfo.toString())
+  tap.equal(parsedUrl.username, 'user%3An%40me')
+  tap.equal(parsedUrl.password, 'p%40ss%3Aword')
+  tap.equal(parsedUrl.hostname, 'github.com')
+
+  // For full backwards-compatibility; support auth where only username or only password is provided
+  tap.equal(HostedGitInfo.fromUrl('https://user%3An%40me@github.com/npm/hosted-git-info.git').auth, 'user%3An%40me')
+  tap.equal(HostedGitInfo.fromUrl('https://:p%40ss%3Aword@github.com/npm/hosted-git-info.git').auth, ':p%40ss%3Aword')
+} else {
+  parsedInfo = HostedGitInfo.fromUrl('https://user%3An%40me:p%40ss%3Aword@github.com/npm/hosted-git-info.git')
+  tap.equal(parsedInfo.auth, 'user:n@me:p@ss:word')
+}
