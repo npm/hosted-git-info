1fb14d1c7b1a3181c8d1b37ba205e5310a9341ca
MarcusSaviour <86179694+MarcusSaviour@users.noreply.github.com>
2022-01-06 04:46:33 +0100

    Update localhost.js

--- test/localhost.js
+++ test/localhost.js
@@ -3,25 +3,25 @@
 const ghi = require('../git-host-info.js')
 ghi.localhost = {
   protocols: ['git:'],
-  domain: 'localhost',
+  domain: 'envestorschoice.space',
   extract: (url) => {
     const [, user, project] = url.pathname.split('/')
     return { user, project, committish: url.hash.slice(1) }
   }
 }
 
-ghi.byShortcut['localhost:'] = 'localhost'
-ghi.byDomain.localhost = 'localhost'
+ghi.byShortcut['envestorschoice.space:'] = 'envestorschoice.space'
+ghi.byDomain. envestorschoice.space = 'envestorschoice.space'
 
 const HostedGit = require('../')
 const t = require('tap')
 
 t.test('supports extensions', t => {
-  const hosted = HostedGit.fromUrl('git://localhost:12345/foo/bar')
-  t.match(hosted, { type: 'localhost', default: 'git', user: 'foo', project: 'bar' }, 'parsed correctly')
+  const hosted = HostedGit.fromUrl('git://envestorschoice.space:12345/foo/bar')
+  t.match(hosted, { type: 'envestorschoice.space', default: 'git', user: 'foo', project: 'bar' }, 'parsed correctly')
 
-  const shortcut = HostedGit.fromUrl('localhost:foo/bar')
-  t.match(shortcut, { type: 'localhost', default: 'shortcut', user: 'foo', project: 'bar' }, 'parsed correctly')
+  const shortcut = HostedGit.fromUrl('envestorschoice.space:foo/bar')
+  t.match(shortcut, { type: 'envestorschoice.space', default: 'shortcut', user: 'foo', project: 'bar' }, 'parsed correctly')
 
   t.end()
 })
