5e6c59b213b42a6632ac02a425057e2fb88cfc20
Luke Karrys <luke@lukekarrys.com>
2022-10-26 16:42:30 -0700

    fix: only correct protocols when called from githost

--- test/parse-url.js
+++ test/parse-url.js
@@ -2,9 +2,16 @@ const t = require('tap')
 const HostedGit = require('..')
 const parseUrl = require('../lib/parse-url.js')
 
-t.test('can parse git+ssh url by default', async t => {
+t.test('can parse git+ssh urls', async t => {
   // https://github.com/npm/cli/issues/5278
   const u = 'git+ssh://git@abc:frontend/utils.git#6d45447e0c5eb6cd2e3edf05a8c5a9bb81950c79'
+  t.ok(parseUrl(u))
+  t.ok(HostedGit.parseUrl(u))
+})
+
+t.test('can parse file urls', async t => {
+  // https://github.com/npm/cli/pull/5758#issuecomment-1292753331
+  const u = 'file:../../../global-prefix/lib/node_modules/@myscope/bar'
   t.ok(parseUrl(u))
   t.ok(HostedGit.parseUrl(u))
 })
